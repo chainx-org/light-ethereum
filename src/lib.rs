4397f8569bbfc47f89fd9cd90aee4865d3e70e0a
gguoss <guanghuaguo.oss@gmail.com>
2018-12-26 16:25:32 +0800

    Add ethereum header

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// Copyright 2018 Chainpool
+extern crate keccak_hash as hash;
+extern crate parity_bytes as bytes;
+extern crate common_types as types;
+extern crate parity_machine;
+
+mod header;
+
+/*
+fn main() {
+    println!("Hello, world!");
+}*/
